de359e5679bf9dc7311ccfe6c4b0140c6dba57f3
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-03-11 17:31:37 -0300

    :sparkles: feat: initial config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
+import '../styles/globals.css'
 import type { AppProps } from "next/app";
+import { League_Spartan } from 'next/font/google'
+
+const leagueSpartan = League_Spartan({
+  weight: ['700', '400'],
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <main className={`${leagueSpartan.className} invoice-dark-bg min-h-screen bg-invoice-light dark:bg-invoice-dark`}>
+    <Component {...pageProps} />;
+  </main>
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,22 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: ['selector', '[data-mode="dark"]'],
   theme: {
+    colors: {
+      "invoice-01": "#7C5DFA",
+      "invoice-02": "#9277FF",
+      "invoice-03": "#1E2139",
+      "invoice-04": "#252945",
+      "invoice-05": "#DFE3FA",
+      "invoice-06": "#888EB0",
+      "invoice-07": "#7E88C3",
+      "invoice-08": "#0C0E16",
+      "invoice-09": "#EC5757",
+      "invoice-10": "#FF9797",
+      "invoice-light": "#F8F8FB",
+      "invoice-dark": "#141625",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
