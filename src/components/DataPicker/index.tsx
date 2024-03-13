4cf8a959198f84d2162ccc49523ee9effd07fb1e
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-03-13 19:57:03 -0300

    :sparkles: feat: adding simple custom input for future

--- src/components/DataPicker/index.tsx
+++ src/components/DataPicker/index.tsx
@@ -4,9 +4,23 @@ import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 import { ptBR } from "date-fns/locale/pt-BR";
+import { InputProps } from "../Input";
+
 registerLocale("ptBR", ptBR);
 
+const InputWithIcon = forwardRef<HTMLInputElement, InputProps>(
+  ({ value, onClick }, ref) => (
+    <input
+      className="px-5 pt-[18px] pb-[15px] border border-invoice-05 rounded-[4px]  focus:ring-1 focus:ring-invoice-01 
+    dark:border-invoice-04 dark:ring-invoice-04 dark:bg-invoice-03 dark:text-white text-[0.9375rem] leading-[0.9375rem] tracking-widest font-bold hover:cursor-pointer focus:outline-none focus:border-invoice-02  disabled:cursor-not-allowed disabled:opacity-50 w-full"
+      onClick={onClick}
+      ref={ref}
+      value={value}
+    />
+  )
+);
 
+InputWithIcon.displayName = "CustomInput";
 
 export const DateComponent = () => {
   const [startDate, setStartDate] = useState(new Date());
@@ -15,11 +29,14 @@ export const DateComponent = () => {
       selected={startDate}
       onChange={(date) => date && setStartDate(date)}
       minDate={new Date()}
-      dayClassName={() => "text-[0.9375rem] leading-[0.9375rem] tracking-widest font-bold"}
+      customInput={<InputWithIcon />}
+      dayClassName={() =>
+        "text-[0.9375rem] leading-[0.9375rem] tracking-widest font-bold"
+      }
       locale="ptBR"
       dateFormat="dd MMM YYYY"
       calendarClassName="bg-white dark:bg-invoice-03 border border-invoice-05 dark:border-invoice-04 rounded-[4px] text-invoice-08 dark:text-white"
-      className="px-5 pt-[18px] pb-[15px] border border-invoice-05 rounded-[4px]  focus:ring-1 focus:ring-invoice-01 dark:border-invoice-04 dark:ring-invoice-04 dark:bg-invoice-03 dark:text-white text-[0.9375rem] leading-[0.9375rem] tracking-widest font-bold hover:cursor-pointer focus:outline-none focus:border-invoice-02  disabled:cursor-not-allowed disabled:opacity-50 w-full"
+      className=""
     />
   );
 };
