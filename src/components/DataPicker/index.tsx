0e44d6e183ca9c920f4ed1e5dd3b8e188bc6f794
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-03-19 21:48:16 -0300

    :sparkles: feat: adding date picker icon

--- src/components/DataPicker/index.tsx
+++ src/components/DataPicker/index.tsx
@@ -6,17 +6,30 @@ import "react-datepicker/dist/react-datepicker.css";
 import { ptBR } from "date-fns/locale/pt-BR";
 import { InputProps } from "../Input";
 
+import IconCalendar from "@/assets/icon-calendar.svg";
+import Image from "next/image";
+
+
 registerLocale("ptBR", ptBR);
 
 const InputWithIcon = forwardRef<HTMLInputElement, InputProps>(
   ({ value, onClick }, ref) => (
-    <input
-      className="px-5 pt-[18px] pb-[15px] border border-invoice-05 rounded-[4px]  focus:ring-1 focus:ring-invoice-01 
-    dark:border-invoice-04 dark:ring-invoice-04 dark:bg-invoice-03 dark:text-white text-[0.9375rem] leading-[0.9375rem] tracking-widest font-bold hover:cursor-pointer focus:outline-none focus:border-invoice-02  disabled:cursor-not-allowed disabled:opacity-50 w-full"
-      onClick={onClick}
-      ref={ref}
-      value={value}
-    />
+    <div>
+      <input
+        className="px-5 pt-[18px] pb-[15px] border border-invoice-05 rounded-[4px]  focus:ring-1 focus:ring-invoice-01 
+      dark:border-invoice-04 dark:ring-invoice-04 dark:bg-invoice-03 dark:text-white text-[0.9375rem] leading-[0.9375rem] tracking-widest font-bold hover:cursor-pointer focus:outline-none focus:border-invoice-02  disabled:cursor-not-allowed disabled:opacity-50 w-full"
+        onClick={onClick}
+        ref={ref}
+        value={value}
+      />
+      <Image 
+        src={IconCalendar}
+        alt="Calendar"
+        width={16}
+        height={16}
+        className="absolute top-1/2 right-5 transform -translate-y-1/2"
+      />
+    </div>
   )
 );
 
